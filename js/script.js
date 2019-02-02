613dbd4bc6e98a60cd0f24c657f7be8159775acc
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2019-02-02 15:34:29 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+/**
+ *
+  -----Evaluations
+  The programs includes the 3 sections; form, table and total
+  The form validates the required inputs
+  The form submits and adds a new bill item
+  The from reset button works
+  The form automatically resets itself after submitting
+  The table display correctly all the bills
+  The table sort by name, type and amount
+  The sort is reversed for each sortable field
+  The total is correctly calculated and update when new bills are added
+  -----Technical Requirements:
+  IFE (Immediately Invoked Function Expression) is used
+  Named functions are used on event handler
+  DOM is created programmatically for the table and total sections
+  Event listeners are added programmatically using JavaScript DOM API
+  Data is saved on an Array and manipulated using Array methods
+  Each bill is saved as an Object
+ * ...
+*/
