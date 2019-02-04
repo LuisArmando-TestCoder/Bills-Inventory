52378f8f9b02e7a65c03040f1de2b95b47f50108
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2019-02-04 17:01:21 -0600

    delete: comments

--- js/billsManager.js
+++ js/billsManager.js
@@ -137,25 +137,3 @@
   tableObj.buttonTypeElem.addEventListener('click', sortTable('ByType').by);
   tableObj.buttonAmountElem.addEventListener('click', sortTable('ByAmount').by);
 }());
-
-/**
- *
-  -----Evaluations
-  -- The programs includes the 3 sections; form, table and total
-  -- The form validates the required inputs
-  -- The form submits and adds a new bill item
-  -- The form reset button works
-  -- The form automatically resets itself after submitting
-  -- The table display correctly all the bills
-  -- The table sort by name, type and amount
-  -- The sort is reversed for each sortable field
-  -- The total is correctly calculated and update when new bills are added
-  -----Technical Requirements:
-  -- IIFE (Immediately Invoked Function Expression) is used
-  -- Named functions are used on event handler
-  -- DOM is created programmatically for the table and total sections
-  -- Event listeners are added programmatically using JavaScript DOM API
-  -- Data is saved on an Array and manipulated using Array methods
-  -- Each bill is saved as an Object
- * ...
-*/
